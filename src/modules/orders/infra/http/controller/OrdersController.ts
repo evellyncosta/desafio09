a40d547b31abe2f4f01ea006940d05d73e6f2889
Evellyn Costa <ecosta.inf@gmail.com>
2020-10-25 16:06:48 -0300

    ajustes

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -4,13 +4,34 @@ import { container } from 'tsyringe';
 
 import CreateOrderService from '@modules/orders/services/CreateOrderService';
 import FindOrderService from '@modules/orders/services/FindOrderService';
+import AppError from '@shared/errors/AppError';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { id } = request.params;
+    if (!id){
+      throw new AppError('id não encontrado');
+    }
+
+    const findOrder = container.resolve(FindOrderService);
+
+    const order = await findOrder.execute({
+      id,
+    });
+
+    return response.json(order);
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { customer_id, products } = request.body;
+
+    const createOrder = container.resolve(CreateOrderService);
+
+    const customer = await createOrder.execute({
+      customer_id,
+      products,
+    });
+
+    return response.json(customer);
   }
 }
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,79 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError(
+        'Não foi possível encontrar nenhum customer com este id',
+      );
+    }
+
+    const existenteProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (!existenteProducts.length) {
+      throw new AppError(
+        'Não foi possível encontrar nenhum produto com este id',
+      );
+    }
+
+    const existenteProductsIds = existenteProducts.map(product => product.id);
+
+    const checkInexsitentProducts = products.filter(
+      product => !existenteProductsIds.includes(product.id),
+    );
+
+    if (checkInexsitentProducts.length) {
+      throw new AppError(`Produtos não encontrados ${checkInexsitentProducts[0].id}`,
+      );
+    }
+
+    const findProductsWithNoQuantityAvailable = products.filter(
+      product =>
+        existenteProducts.filter(p => p.id === product.id)[0].quantity <=
+        product.quantity,
+    );
+
+    if (findProductsWithNoQuantityAvailable) {
+      throw new AppError(
+        `A quantidade de produtos não está disponível  para compra.`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existenteProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderedProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existenteProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
   }
 }
 
